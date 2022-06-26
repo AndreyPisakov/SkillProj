21fe0ae8825b0730dba16c47284f7de89a227d77
pisakov <11234897a@gmail.com>
2022-06-26 16:39:05 +0300

    Added menu buttons, movie posters, text fields, background, CardView for rounded corners and levitation, functionality clicks of each button with display of Toast-messages.

--- app/src/main/java/com/pisakov/skillproj/MainActivity.kt
+++ app/src/main/java/com/pisakov/skillproj/MainActivity.kt
@@ -2,10 +2,24 @@ package com.pisakov.skillproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.pisakov.skillproj.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnMenu.setOnClickListener { Toast.makeText(this@MainActivity, R.string.menu, Toast.LENGTH_SHORT).show() }
+            btnFavor.setOnClickListener { Toast.makeText(this@MainActivity, R.string.favor, Toast.LENGTH_SHORT).show() }
+            btnLater.setOnClickListener { Toast.makeText(this@MainActivity, R.string.later, Toast.LENGTH_SHORT).show() }
+            btnSelections.setOnClickListener { Toast.makeText(this@MainActivity, R.string.selections, Toast.LENGTH_SHORT).show() }
+            btnSettings.setOnClickListener { Toast.makeText(this@MainActivity, R.string.settings, Toast.LENGTH_SHORT).show() }
+        }
     }
 }
