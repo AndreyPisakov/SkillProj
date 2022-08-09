434c054b0745f029184b6bd6ef18f4f38aa0e41c
pisakov <11234897a@gmail.com>
2022-08-09 15:17:58 +0300

    Added animations

---
+++ app/src/main/java/com/pisakov/skillproj/LaterFragment.kt
@@ -0,0 +1,26 @@
+package com.pisakov.skillproj
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pisakov.skillproj.databinding.FragmentLaterBinding
+
+class LaterFragment : Fragment() {
+
+    private lateinit var binding: FragmentLaterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_later, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLaterBinding.bind(view)
+        AnimationHelper.performFragmentCircularRevealAnimation(binding.laterFragmentRoot, requireActivity(), 3)
+    }
+}
