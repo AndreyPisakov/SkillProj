ef29001b0b4c37cd9695fe14e5e0ea46b9f4b8c6
pisakov <11234897a@gmail.com>
2022-12-25 21:21:56 +0300

    Added receiver

--- app/src/main/java/com/pisakov/skillproj/view/MainActivity.kt
+++ app/src/main/java/com/pisakov/skillproj/view/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.pisakov.skillproj.view
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.pisakov.skillproj.R
 import com.pisakov.skillproj.databinding.ActivityMainBinding
+import com.pisakov.skillproj.receivers.ConnectionChecker
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +22,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val navController = findNavController(R.id.myNavHostFragment)
         binding.bottomNavigation.setupWithNavController(navController)
+
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
